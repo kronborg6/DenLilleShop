05d7e1cbe78606112cb5915ffa5b612688970fb7
Kronborg <mkronborg7@gmail.com>
2020-10-12 19:48:10 +0200

    0.3.0
    + kan oprette Kunder
    + kan gå tilbage til menu (8)

--- DenLilleShop/DenLilleShop/Program.cs
+++ DenLilleShop/DenLilleShop/Program.cs
@@ -23,7 +23,37 @@ namespace DenLilleShop
                         switch (ind)
                         {
                             case 1:
-                                Customer.Add(new Customer());
+                                Console.Clear();
+                                Console.WriteLine("Opret Kunde");
+                                Console.WriteLine("ID");
+                                int ID = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Fornavn");
+                                string Fname = Console.ReadLine();
+                                Console.WriteLine("EfterNavn");
+                                string Ename = Console.ReadLine();
+                                Console.WriteLine("Mobil Nummer");
+                                int MobilN = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Email");
+                                string Em = Console.ReadLine();
+                                Console.WriteLine("y/n vil du gemme det??");
+                                string YN = Console.ReadLine().ToLower();
+
+                                if (YN == "y")
+                                {
+                                    customers.Add(new Customer(ID, Fname, Ename, MobilN, Em));
+                                    Console.WriteLine("Kunde er blevet gemt");
+                                    Console.WriteLine("Kunde ID: ");
+                                    Console.WriteLine("Navn: ");
+                                    Console.WriteLine("Mobil Nummer: ");
+                                    Console.WriteLine("Email: ");
+                                    Console.WriteLine("\n\n1. vis du vil oprette en kunde til");
+                                    Console.WriteLine("8. vis du vil tilbage til Menuen");
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                                
                                 break;
                             case 2:
                                 Console.WriteLine("Hej med digg");
@@ -38,6 +68,8 @@ namespace DenLilleShop
                             case 7:
                                 break;
                             case 8:
+                                Console.Clear();
+                                m.ListMenu();
                                 break;
                             default:
                                 break;
